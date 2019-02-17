7ad55d9cc1f6ac3c4283aacb99bfe13e81f97024
gabriel loye <gabrielloye@hotmail.com>
2019-02-17 22:31:40 +0800

    skills segment done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ faEnvelope, faHome, faBookOpen, faCodeBranch, faUser,
 faGraduationCap, faBriefcase, faChild, faStar, faSwimmer,
 faHeadphones, faFutbol, faMusic, faCode, faLanguage, faMale,
 faGlobeAsia, faPlay, faBars, faPencilAlt, faPenAlt, faSchool,
-faUniversity, faTools, faToolbox } from '@fortawesome/free-solid-svg-icons';
-import { faLightbulb, faKeyboard } from '@fortawesome/free-regular-svg-icons';
+faUniversity, faTools, faToolbox, faFlask, faProjectDiagram } from '@fortawesome/free-solid-svg-icons';
+import { faLightbulb, faKeyboard, faEdit } from '@fortawesome/free-regular-svg-icons';
 import { } from '@fortawesome/free-brands-svg-icons'
 
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -63,6 +63,7 @@ export class AppModule {
     faUser, faGraduationCap, faBriefcase, faChild, faStar, faSwimmer,
     faHeadphones, faFutbol, faMusic, faCode, faLanguage, faMale,
     faGlobeAsia, faPlay, faBars, faPencilAlt, faPenAlt, faSchool, 
-    faUniversity, faTools, faLightbulb, faToolbox, faKeyboard)
+    faUniversity, faTools, faLightbulb, faToolbox, faKeyboard, faFlask,
+    faEdit, faProjectDiagram)
   }
 }
